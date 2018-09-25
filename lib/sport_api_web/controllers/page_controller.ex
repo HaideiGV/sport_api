e86ebfe6b381cc62922be135804f4c10cf3a7059
Viacheslav Haidei <haideigv@gmail.com>
2018-09-25 08:01:09 +0300

    Added filtering by division.

--- lib/sport_api_web/controllers/page_controller.ex
+++ lib/sport_api_web/controllers/page_controller.ex
@@ -5,20 +5,39 @@ defmodule SportApiWeb.PageController do
     render conn, "index.html"
   end
 
+  def parsed_season_pair(season_string) do
+    first = String.slice(season_string, 0, 4)
+    second = String.slice(season_string, 0, 2)
+    third = String.slice(season_string, 4, 6)
+    "#{first}-#{second}#{third}"
+  end
+
+  # def filter_row(row, filter_map) do
+  #   Enum.map(filter_map, fn({key, value}) -> Map.get(raw, key) == value)
+  # end
+
   def get_leagues(%Plug.Conn{params: params} = conn, _params) do
     data = File.stream!(Path.absname("data.csv")) |> CSV.decode
     data = data
     |> Enum.slice(1, Enum.count(data))
     |> Enum.map( 
-      fn ({:ok, [id, _div, season, date, home_team, away_team, _fthg, _ftag, _ftr, _hthg, _htag, _htr]}) -> %{
-        "id" => id, "date" => date, "pair" => "#{home_team} #{away_team}",
-        "season" => "#{String.slice(season, 0, 4)}-#{String.slice(season, 0, 2) <> String.slice(season, 4, 6)}"
+      fn ({:ok, [id, div, season, date, home_team, away_team, _fthg, _ftag, _ftr, _hthg, _htag, _htr]}) -> %{
+        "id" => id, 
+        "date" => date, 
+        "pair" => "#{home_team} #{away_team}",
+        "division" => div,
+        "season" => parsed_season_pair(season)
       }
     end)
 
     request_season = Map.get(params, "season")
+    request_division = Map.get(params, "division")
     if request_season do
       json conn, Enum.filter(data, fn(row) -> Map.get(row, "season") == request_season end)
+    end
+
+    if request_division do
+      json conn, Enum.filter(data, fn(row) -> Map.get(row, "division") == request_division end)
     end
 
     json conn, data
